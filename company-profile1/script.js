1478f255c04accc5b4f1a15129207e9fbdc41c9d
slowy07 <slowy.arfy@gmail.com>
2021-06-14 05:25:09 +0700

    script js

---
+++ company-profile1/script.js
@@ -0,0 +1,5 @@
+//side navigation header
+const sidenav = document.querySelectorAll('.sidenav');
+M.Sidenav.init(sidenav)
+const parallax = document.querySelectorAll('.parallax');
+M.Parallax.init(parallax)
