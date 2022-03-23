a443e0d7c952f206805e80ab4d488edd071a0997
arfy slowy <slowy.arfy@gmail.com>
2022-03-23 12:24:59 +0700

    Update include.js

--- include.js
+++ include.js
@@ -1,11 +1,20 @@
-var i = 0;
-var text = "Computer scientist";
-function typing() {
-    if (i < text.length) {
-        document.getElementById("running-text").innerHTML += text.charAt(i);
-        i++;
-        setTimeout(typing, 300);
-    }
-    
-}
-typing();
+$(function(){
+	$(".typed").typed({
+		strings: ["Computer Scientist", "Software Engineer"],
+		stringsElement: null,
+		typeSpeed: 30,
+		startDelay: 1200,
+		backSpeed: 20,
+		backDelay: 500,
+		loop: true,
+		loopCount: false,
+		showCursor: false,
+		cursorChar: "|",
+		attr: null,
+		contentType: 'html',
+		callback: function() {},
+		preStringTyped: function() {},
+		onStringTyped: function() {},
+		resetCallback: function() {}
+	});
+});
